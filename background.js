ec9bc724bc7be448e061c74b3da5f97722510f77
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-10-19 13:21:12 -0700

    Change to unmute if user returns to page

--- background.js
+++ background.js
@@ -1,8 +1,27 @@
 // Destroy audible, inactive tabs
 
-chrome.tabs.onUpdated.addListener((id, info, tab) => {
-  if(info["audible"] === true) {
+function muteTab(tab){
+  if(tab["audible"] && !tab["highlighted"]) {
     console.log(tab)
-    chrome.tabs.update(id, {"muted": true})
+    chrome.tabs.update(tab["id"], {"muted": true})
   }
+  else {
+    chrome.tabs.update(tab["id"], {"muted": false})
+  }
+}
+
+// Other tab starts playing sound
+
+chrome.tabs.onUpdated.addListener((id, info, tab) => {
+  muteTab(tab)
+})
+
+// User changes tab, mute other tabs
+
+chrome.tabs.onHighlightChanged.addListener((info) => {
+  chrome.tabs.query({"audible": true}, (tabs) => {
+    for(tab of tabs) {
+      muteTab(tab)
+    }
+  })
 })
