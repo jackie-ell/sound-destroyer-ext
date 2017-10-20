a97d73b6e16e5f464522fe95fcb37638914e339a
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-10-20 11:59:25 -0700

    Fix asnyc isTrusted not resolving before if statement

--- background.js
+++ background.js
@@ -1,24 +1,31 @@
 // Returns true if site is trusted, otherwise false
 
 function isTrusted(url) {
-
-  chrome.storage.sync.get(url, (items) => {
-    if(items[url]) {
-      return true
-    }
-    return false
+  return new Promise((res, rej) => {
+    chrome.storage.sync.get(url, (items) => {
+      if(items[url]) {
+        res(true)
+      } else {
+        rej(false)
+      }
+    })
   })
 }
 
 // Destroy audible, inactive, distrusted tabs
 
 function muteTab(tab){
-  if(tab["audible"] && !tab["highlighted"] && isTrusted(tab["url"])) {
-    chrome.tabs.update(tab["id"], {"muted": true})
-  }
-  else {
-    chrome.tabs.update(tab["id"], {"muted": false})
-  }
+  isTrusted(tab["url"])
+    .then(() => {
+      chrome.tabs.update(tab["id"], {"muted": false})
+    })
+    .catch(() => {
+      if(tab["audible"] && !tab["highlighted"]) {
+        chrome.tabs.update(tab["id"], {"muted": true})
+      } else {
+        chrome.tabs.update(tab["id"], {"muted": false})
+      }
+    })
 }
 
 // Other tab starts playing sound
